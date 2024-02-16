8f21e52814bf45f882f5c0f309834ea1db290924
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-16 18:16:42 +0545

    creating server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("kismat love everyone")
+const http = require('http');
+const fs = require("fs");
+
+const myServer = http.createServer((req, res)=>{
+    const log=`${Date.now()}: ${req.url} New req received`;
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(req.url){
+            case '/':res.end("Home page ")
+        }
+        res.end("Hello from server")
+    })
+    console.log("New req ");
+    res.end("Hello from server")
+})
+
+myServer.listen(3000,()=> console.log("Server started"));
