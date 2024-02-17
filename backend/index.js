5d61910ad2c5d2413d3d13438389a65d58e5ed37
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-17 15:37:08 +0545

    passing query parameters

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,30 @@
 const http = require('http');
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res)=>{
     const log=`${Date.now()}: ${req.url} New req received`;
+    const myUrl= url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
-            case '/':res.end("Home page ")
+        switch(myUrl.pathname){
+            case "/":res.end("Home page ")
+            break;
+            case "/about":
+            const username = myUrl.query.name;
+            res.end(`hii, ${username}`)
+            break;
+            case "/search":
+                const search = myUrl.query.search_query
+                res.end("Here are your results for" + search)
+                break;
+            default:
+                res.sendDate("404 not found");
         }
         res.end("Hello from server")
     })
     console.log("New req ");
-    res.end("Hello from server")
+    // res.end("Hello from server")
 })
 
 myServer.listen(3000,()=> console.log("Server started"));
