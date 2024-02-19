1cf07c92af1af0aad6861cc539925e8ecbadc2ce
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-19 14:03:17 +0545

    express used instead of http

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,12 @@
 const http = require('http');
-const fs = require("fs");
-const url = require("url");
 
-const myServer = http.createServer((req, res)=>{
-    const log=`${Date.now()}: ${req.url} New req received`;
-    const myUrl= url.parse(req.url, true);
-    console.log(myUrl);
-    fs.appendFile("log.txt",log,(err,data)=>{
-        switch(myUrl.pathname){
-            case "/":
-            if(req.method === 'GET') res.end("/homepage")
-            break;
-            case "/about":
-            const username = myUrl.query.name;
-            res.end(`hii, ${username}`)
-            break;
-            case "/search":
-                const search = myUrl.query.search_query
-                res.end("Here are your results for" + search)
-                break;
-            case "/signup":
-                if(req.method === 'GET') res.end('This is a signup form'); 
-                 else if(req.method === 'POST') {
-                    res.end("success")
-                 }
-            default:
-                res.sendDate("404 not found");
-        }
-        res.end("Hello from server")
-    })
-    console.log("New req ");
-    // res.end("Hello from server")
+const express = require('express');
+
+const app= express()
+
+app.get("/",(req,res)=>{
+  res.end("siuuuu")
 })
 
-myServer.listen(3000,()=> console.log("Server started"));
+// const myServer = http.createServer(app)
+app.listen(3000,()=> console.log("Server started"));
