8d6a3c83388da004de0d18e9d5c29dfb02ddbea3
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-20 15:44:51 +0545

    mongo db server startde

--- backend/mongo.js
+++ backend/mongo.js
@@ -5,7 +5,11 @@ const express = require('express');
 
 const app= express()
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://localhost:27017').then(()=>{
+    console.log("mongodb connected")
+}).catch((err)=>{
+    console.log("Mongo Error", err);
+})
 
 const userSchema = new mongoose.Schema({
     firstName:{
