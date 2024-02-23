6d515f0cde65ce1c3b29cc40ec9f36ec6f097278
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-23 10:54:51 +0545

    server setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 const http = require('http');
 
 const express = require('express');
+const path = require("path");
 
 const app= express()
 
-app.get("/",(req,res)=>{
-  res.end("siuuuu")
-})
+const staticRoute = require('./routes/staticRouter')
+const userRoute = require('./routes/user')
+
+app.use(express.json());
+
+app.use("/user",userRoute)
+app.use("/",staticRoute);
+
+// app.get("/",(req,res)=>{
+//   res.end("siuuuu")
+// })
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
 
 // const myServer = http.createServer(app)
 app.listen(3000,()=> console.log("Server started"));
