a7f891e4dd7f576ee7935a730dc2b976b694f067
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-28 19:51:30 +0545

    integrated both schema and database

--- practise/index.js
+++ practise/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
+import express from 'express'
+import User from "./models/User.js"
+import db from "./connection.js"
 
 const app = express ()
+
+app.use(express.json())
+
+db();
 
 
 
 app.post('/cart',(req,res)=>{
-    
+    const { name} =  req.body;
+    console.log(id,name,email)
+    const user = new User({name})
+    user.save().then((data)=>{
+    res.status(200).json(data)
+    }).catch((err)=>{
+        res.status(500).json(err);
+    })
 })
 app.listen(8000,()=>{
     console.log("server running 8000")
