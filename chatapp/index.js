5ae4a4d9f8ae7e37e5a0e84a3d17a1e7d57d7070
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-29 18:48:31 +0545

    creating socket server

---
+++ chatapp/index.js
@@ -0,0 +1,25 @@
+const http = require("http")
+const express = require("express");
+const path = require('path')
+
+const {Server} = require("socket.io");
+
+const app = express()
+const server = http.createServer(app);
+const io = new Server(server)
+
+// Socket io handle
+
+
+app.use(express.static(path.resolve('./public')))
+
+app.get('/',(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
+
+server.listen(9000, ()=>{
+    console.log(`server conected at 9000`)
+})
+
+
+
