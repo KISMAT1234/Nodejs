5681ba46da3ac4f5184d38968c390a16bf8e6e44
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-07 11:03:39 +0545

    send msg to server

--- whatsapp/public/client.js
+++ whatsapp/public/client.js
@@ -1,31 +1,53 @@
 const socket = io()
-
 let name;
-
-let messageArea = document.querySelector('#message_area')
 let textarea = document.querySelector('#textarea')
-do{
-    name= prompt('Please enter your name')
-}
-while(!name)
+let messageArea = document.querySelector('.message__area')
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
 
-textarea.addEventListener('keyup',(e) =>{
-    if(e.key == 'Enter'){
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
         sendMessage(e.target.value)
     }
 })
 
-function sendMessage(msg){
+function sendMessage(message) {
     let msg = {
-        user:name,
-        message: msg
+        user: name,
+        message: message.trim()
     }
+    // Append 
     appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('send', msg)
+
 }
 
-function appendMessage(msg, type){
-     let mainDiv = document.createElement('div')
-     let className= type
-     mainDiv.classList.add(className, 'message')
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
 
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+
+socket.on('receive', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+    // console.log(msg)
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
 }
