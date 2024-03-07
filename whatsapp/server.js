39db8231a1d8783c769e095da246bc5b8578a4e3
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-07 11:03:22 +0545

    emitting message to client

--- whatsapp/server.js
+++ whatsapp/server.js
@@ -24,6 +24,8 @@ const {Server} = require("socket.io");
 const io = new Server(http)
 
 io.on('connection',(socket)=>{
-    console.log(`connected`);
+    socket.on('send',(msg)=>{
+        socket.broadcast.emit('receive', msg)
+    })
 })
 
